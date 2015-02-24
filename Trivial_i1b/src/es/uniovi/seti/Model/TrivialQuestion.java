6395681c40fa8f13904c93cc3468510440bcea19
anareces <anareces@hotmail.com>
2015-02-24 16:10:41 +0100

    Update TrivialQuestion.java
    New provisional question interface with Trivial question class.

--- Trivial_i1b/src/es/uniovi/seti/Model/TrivialQuestion.java
+++ Trivial_i1b/src/es/uniovi/seti/Model/TrivialQuestion.java
@@ -1,5 +1,29 @@
 package es.uniovi.seti.Model;
 
 public class TrivialQuestion {
-
+  
+  private String question; 
+  
+  private List<String> answers;
+  
+  private int positionTrue;
+  
+  public TrivialQuestion(String question, List<String> answers, int positionTrue){
+      this.question = question;
+      this.answers = answers;
+      this.positionTrue = positionTrue;
+  }
+  
+  public String getQuestion(){
+    return this.question;
+  }
+  
+  public List<String> getAnswers(){
+    return this.answers;
+  }
+  
+  public int getPositionTrue(){
+    return this.positionTrue;
+  }
+  
 }
