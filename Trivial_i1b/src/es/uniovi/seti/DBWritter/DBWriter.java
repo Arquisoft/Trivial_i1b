bf9f07c2570c162294972a41c5198c41146488d7
Raquel <arrojoraquel@gmail.com>
2015-03-04 12:41:40 +0100

    Database added again

--- Trivial_i1b/src/es/uniovi/seti/DBWritter/DBWriter.java
+++ Trivial_i1b/src/es/uniovi/seti/DBWritter/DBWriter.java
@@ -2,4 +2,5 @@ package es.uniovi.seti.DBWritter;
 
 public interface DBWriter {
 
+	void insertQuestions();
 }
--- Trivial_i1b/src/es/uniovi/seti/DataBase/MongoDBConnector.java
+++ Trivial_i1b/src/es/uniovi/seti/DataBase/MongoDBConnector.java
@@ -1,5 +1,27 @@
 package es.uniovi.seti.DataBase;
 
-public class MongoDBConnector implements DBConnector {
+import java.net.UnknownHostException;
 
+import com.mongodb.MongoClient;
+
+public class MongoDBConnector implements DBConnector {
+	private MongoClient mongo = null;
+	
+	public MongoDBConnector(){}
+
+	private void createConexion() {
+		try {
+			mongo = new MongoClient("localhost", 27017);
+		} catch (UnknownHostException e) {
+			System.err
+					.println("No se ha podido establecer la conexion con la base");
+		}
+
+	}
+
+	public MongoClient getConexion() {
+		if (mongo == null)
+			createConexion();
+		return mongo;
+	}
 }
