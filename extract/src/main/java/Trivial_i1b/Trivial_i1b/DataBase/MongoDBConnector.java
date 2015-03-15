e088a726308c9846ec4cef854fa0f11929595da3
uo230364 <ivansvag@hotmail.com>
2015-03-15 19:36:07 +0100

    Merging extract and project folders

--- extract/src/main/java/Trivial_i1b/Trivial_i1b/DataBase/MongoDBConnector.java
+++ extract/src/main/java/Trivial_i1b/Trivial_i1b/DataBase/MongoDBConnector.java
@@ -1,7 +1,5 @@
 package Trivial_i1b.Trivial_i1b.DataBase;
 
-import java.net.UnknownHostException;
-
 import com.mongodb.MongoClient;
 
 public class MongoDBConnector implements DBConnector {
@@ -10,12 +8,7 @@ public class MongoDBConnector implements DBConnector {
 	public MongoDBConnector(){}
 
 	private void createConnection() {
-		try {
-			mongo = new MongoClient("localhost", 27017);
-		} catch (UnknownHostException e) {
-			System.err
-					.println("The connection to the database could not be established");
-		}
+		mongo = new MongoClient("localhost", 27017);
 
 	}
 
--- extract/src/main/java/Trivial_i1b/Trivial_i1b/Parser/XMLParser.java
+++ extract/src/main/java/Trivial_i1b/Trivial_i1b/Parser/XMLParser.java
@@ -1,17 +1,14 @@
 package Trivial_i1b.Trivial_i1b.Parser;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 import Trivial_i1b.Trivial_i1b.Model.Question;
 import Trivial_i1b.Trivial_i1b.Model.TrivialQuestion;
