8909efd5dd92edca74b1a42a4d2ef49cef374cc7
David Casado <davidcasado@outlook.com>
2015-04-02 16:52:30 +0200

    Changes in MongoDBWriter and Die created

--- extract/src/main/java/DBWriter/MongoDBWriter.java
+++ extract/src/main/java/DBWriter/MongoDBWriter.java
@@ -2,58 +2,61 @@ package DBWriter;
 
 import java.util.List;
 
+import org.bson.Document;
+
 import DataBase.MongoDBConnector;
 import Model.Question;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class MongoDBWriter implements DBWriter {
-	
+
 	private MongoClient mongo;
 	private List<Question> trivialQuestions;
-	private DB database;
-	private DBCollection table;
-	
-	public MongoDBWriter(List<Question> trivialQuestions) {
-		this.trivialQuestions=trivialQuestions;
-	}
-	
-	public void insertQuestions(){
-		mongo=new MongoDBConnector().getConnection();
-		if (mongo!=null){
-			createElements();
-			addQuestions(trivialQuestions);
-		}
-		else{
-			throw new IllegalStateException("The connection to the database has not beem estabished");
-		}
-	}
-	
-	private void createElements(){
-		database=mongo.getDB("Trivial");
-		table=database.getCollection("question");
-	}
-	
-	private void addQuestions(List<Question>questions){
-		BasicDBObject trivialQuestion;
-		for (Question question: questions){
-			trivialQuestion=createEntries(question);
-			table.insert(trivialQuestion);		
-		}
-	}
-	
-	private BasicDBObject createEntries(Question question){
-		BasicDBObject questionToInsert=new BasicDBObject();
+	private MongoDatabase database;
+	private MongoCollection<Document> table;
 
-		List<String>answers=question.getAnswers();
+	public MongoDBWriter(List<Question> trivialQuestions) {
+		this.trivialQuestions = trivialQuestions;
+	}
+
+	public void insertQuestions() {
+		mongo = new MongoDBConnector().getConnection();
+		if (mongo != null) {
+			createElements();
+			addQuestions();
+		} else {
+			throw new IllegalStateException(
+					"The connection to the database has not beem estabished");
+		}
+	}
+
+	private void createElements() {
+		database = mongo.getDatabase("localhost");
+		database.createCollection("questions");
+		table = database.getCollection("question");
+	}
+
+	private void addQuestions() {
+		Document trivialQuestion;
+		for (Question question : trivialQuestions) {
+			trivialQuestion = createEntries(question);
+			table.insertOne(trivialQuestion);
+		}
+	}
+
+	private Document createEntries(Question question) {
+		Document questionToInsert = new Document();
+
+		List<String> answers = question.getAnswers();
 		questionToInsert.put("question", question.getQuestion());
-		questionToInsert.put("correct", answers.remove(question.getPositionTrue()));
+		questionToInsert.put("correct",
+				answers.remove(question.getPositionTrue()));
 		questionToInsert.put("wrong1", answers.get(0));
 		questionToInsert.put("wrong2", answers.get(1));
-		
+
 		return questionToInsert;
 	}
 }
