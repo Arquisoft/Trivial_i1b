952e4c174f21f14705e51655c043b28e29eaf1db
David <davidcasado@outlook.com>
2015-04-06 14:33:02 +0200

    Method updateUser made and changes in UserManager

--- Game/src/main/java/logic/MongoUserManager.java
+++ Game/src/main/java/logic/MongoUserManager.java
@@ -14,7 +14,7 @@ public class MongoUserManager {
 
 	private static final String COLLECTION_NAME = "users";
 	private static final String DATABASE_NAME = "game";
-	
+
 	private static MongoClient mongo;
 	private static MongoDatabase db;
 	private static MongoCollection<Document> table;
@@ -25,12 +25,13 @@ public class MongoUserManager {
 			if (getUser(user.getUsername()) != null)
 				table.insertOne(createDocument(user));
 			else
-				System.err.println("User with username " + user.getUsername() + " alredy exists.");
-			closeDatabase();
+				System.err.println("User with username " + user.getUsername()
+						+ " alredy exists.");
 			return true;
 		} catch (Exception e) {
-			closeDatabase();
 			return false;
+		} finally {
+			closeDatabase();
 		}
 	}
 
@@ -39,29 +40,51 @@ public class MongoUserManager {
 			connectDatabase();
 			List<Document> docs = new ArrayList<Document>();
 			for (User user : users) {
-				if(getUser(user.getUsername()) != null)
+				if (getUser(user.getUsername()) != null)
 					docs.add(createDocument(user));
 				else
-					System.err.println("User with username " + user.getUsername() + " alredy exists.");
+					System.err.println("User with username "
+							+ user.getUsername() + " alredy exists.");
 			}
 			table.insertMany(docs);
-			closeDatabase();
 			return true;
 		} catch (Exception e) {
-			closeDatabase();
 			return false;
+		} finally {
+			closeDatabase();
 		}
 	}
 
 	public static User getUser(String username) {
-		connectDatabase();
-		BasicDBObject query = new BasicDBObject();
-		query.put("user", username);
-		User user = null;
-		for (Document doc : table.find(query))
-			user = createUser(doc);
-		closeDatabase();
-		return user;
+		try {
+			connectDatabase();
+			BasicDBObject query = new BasicDBObject().append("username",
+					username);
+			User user = null;
+			for (Document doc : table.find(query))
+				user = createUser(doc);
+			return user;
+		} catch (Exception e) {
+			return null;
+		} finally {
+			closeDatabase();
+		}
+	}
+
+	public static boolean updateUser(User user) {
+		try {
+			connectDatabase();
+			BasicDBObject query = new BasicDBObject().append("username",
+					user.getUsername());
+			Document document = new Document().append("$set",
+					createDocument(user));
+			table.updateOne(query, document);
+			return true;
+		} catch (Exception e) {
+			return false;
+		} finally {
+			closeDatabase();
+		}
 	}
 
 	private static User createUser(Document doc) {
