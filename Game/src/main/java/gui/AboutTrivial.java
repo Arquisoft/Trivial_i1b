9e51e3897563dac9f598e86e734359bf863317ae
Álvaro Palanco Lorenzo <alvaropalancolorenzo@gmail.com>
2015-04-08 19:18:31 +0200

    Added a new window that shows the main information about the project and the webpages of it

---
+++ Game/src/main/java/gui/AboutTrivial.java
@@ -0,0 +1,77 @@
+package interfaz;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JTextArea;
+import java.awt.Font;
+
+public class AboutTrivial extends JFrame {
+
+	private JPanel contentPane;
+	private JLabel lbTitle;
+	private JTextArea txtrThisProjectIs;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AboutTrivial frame = new AboutTrivial();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AboutTrivial() {
+		setTitle("Trivial - Software Architecture");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(GameWindow.class.getResource("/Images/icono.png")));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 1500, 750);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 0, 139));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		contentPane.add(getLbTitle());
+		contentPane.add(getTxtrThisProjectIs());
+	}
+
+	private JLabel getLbTitle() {
+		if (lbTitle == null) {
+			lbTitle = new JLabel("");
+			lbTitle.setIcon(new ImageIcon(AboutTrivial.class.getResource("/Images/tituloSmall.png")));
+			lbTitle.setBounds(52, 30, 726, 160);
+		}
+		return lbTitle;
+	}
+	private JTextArea getTxtrThisProjectIs() {
+		if (txtrThisProjectIs == null) {
+			txtrThisProjectIs = new JTextArea();
+			txtrThisProjectIs.setFont(new Font("Arial Black", Font.BOLD, 13));
+			txtrThisProjectIs.setForeground(new Color(255, 255, 255));
+			txtrThisProjectIs.setBackground(new Color(0, 0, 139));
+			txtrThisProjectIs.setEditable(false);
+			txtrThisProjectIs.setLineWrap(true);
+			txtrThisProjectIs.setWrapStyleWord(true);
+			txtrThisProjectIs.setText("This project is developed for the Software Architecture course.\r\nIt's developed by:\r\n-Ana Areces Gonz\u00E1lez\r\n-Mar\u00EDa Gonz\u00E1lez Gonz\u00E1lez\r\n-\u00C1lvaro Palanco Lorenzo\r\n-David Casado Corona\r\n-Iv\u00E1n S\u00E1nchez Vald\u00E9s\r\n-Raquel Arrojo L\u00F3pez\r\n\r\n\r\n\r\n\r\n\r\n\r\nMore information about the project can be found on:\r\nhttps://github.com/Arquisoft/Trivial_i1b    and     http://arquisoft.github.io/Trivial_i1b/\r\n");
+			txtrThisProjectIs.setBounds(62, 201, 933, 401);
+		}
+		return txtrThisProjectIs;
+	}
+}
