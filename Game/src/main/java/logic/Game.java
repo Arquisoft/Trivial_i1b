dbde0944983582b043d3b6cfbdd22af00ae23683
anareces <anareces@hotmail.com>
2015-04-09 21:55:39 +0200

    Changes of players in game

--- Game/src/main/java/logic/Game.java
+++ Game/src/main/java/logic/Game.java
@@ -1,45 +1,61 @@
 package logic;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
-import Model.Question;
 import logic.DB.MongoQuestionManager;
 import logic.board.Board;
 import logic.model.Player;
+import Model.Question;
 
 public class Game {
 	
 	private Board board;
 	
-	private Player[] players;
+	private List<Player> players;
 
 	private Player activePlayer;
 	
-	public Game(int numPlayers){
+	public Game(){
 		
 		List<Question> questions = new MongoQuestionManager().getQuestions();
 		board = new Board(new QuestionSelector(questions));
-		players = new Player[numPlayers];
-		activePlayer = players[0];
+		players = new ArrayList<Player>();
 	}
 	
 	public Board getBoard() {
 		return board;
 	}
+	
+	public void addPlayer(Player player){
+		if(players.isEmpty()){
+			activePlayer = player;
+		}
+		players.add(player);
+	}
 
-	public void nextPlayer() {
-		for (int i = 0; i < players.length; i++) {
-			if (players[i].equals(activePlayer)) {
-				if (i + 1 < players.length)
-					activePlayer = players[i + 1];
+	public Player nextPlayer() {
+		for (int i = 0; i < players.size(); i++) {
+			if (players.get(i).equals(activePlayer)) {
+				if (i + 1 < players.size())
+					activePlayer = players.get(i + 1);
 				else
-					activePlayer = players[0];
+					activePlayer = players.get(0);
 			}
 		}
+		return activePlayer;
 	}
 	
 	public boolean trueAnswer(Question question,int answer){
 		return question.getPositionTrue() == answer; 
+	}
+
+	public List<Player> getPlayers() {
+		return players;
+	}
+
+	public Player getActivePlayer() {
+		return activePlayer;
 	}
 }
