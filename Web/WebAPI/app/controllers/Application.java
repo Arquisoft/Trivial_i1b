3d2d1c5ec7f3af9702cecdd3f912e71d66191af2
anareces <anareces@hotmail.com>
2015-05-05 12:40:15 +0200

    initial transitions

--- Web/WebAPI/app/controllers/Application.java
+++ Web/WebAPI/app/controllers/Application.java
@@ -10,5 +10,13 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
+    
 
+    public static Result initial() {
+        return ok(initial.render());
+    }
+    
+    public static Result board(){
+    	return ok(board.render());
+    }
 }
