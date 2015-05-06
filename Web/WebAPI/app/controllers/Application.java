a78b2a7f5f81a5cf523adf22eb36eb2d9ac45d3a
anareces <anareces@hotmail.com>
2015-05-06 20:42:03 +0200

    not working

--- Web/WebAPI/app/controllers/Application.java
+++ Web/WebAPI/app/controllers/Application.java
@@ -1,8 +1,10 @@
 package controllers;
 
 import model.Game;
+import model.board.square.SimpleSquare;
 import model.board.square.Square;
 import model.model.Player;
+import model.model.Position;
 import model.model.Statistics;
 import model.model.User;
 import play.mvc.Controller;
@@ -13,7 +15,7 @@ import views.html.initial;
 public class Application extends Controller {
 
 	private static Game game = new Game();
-	private static Square[] squares;
+	private static Square[] squares = {new SimpleSquare(null,new Position(1,3)),new SimpleSquare(null,new Position(1,1)) };
 	
     public static Result initial() {
         return ok(initial.render());
@@ -45,7 +47,6 @@ public class Application extends Controller {
     
     public boolean isActive(String id){
     	for(Square s: squares){
-    		System.out.println(s.toString()+ "HOLIIIIIIIIIIII");
     		if(s.toString().equals(id))
     			return true;
     	}
